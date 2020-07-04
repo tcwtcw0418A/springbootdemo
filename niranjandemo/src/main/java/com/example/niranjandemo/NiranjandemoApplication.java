d6100bec293a693f4c77635249ecee8877b21a7b
lenovo <lenovo@192.168.1.2>
2020-07-04 06:50:37 +0530

    initial code commit

---
+++ niranjandemo/src/main/java/com/example/niranjandemo/NiranjandemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.niranjandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiranjandemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiranjandemoApplication.class, args);
+	}
+
+}
---
+++ niranjandemo/src/test/java/com/example/niranjandemo/NiranjandemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.niranjandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiranjandemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
