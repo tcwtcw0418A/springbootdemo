9a5c01d9f9253b6c080145cb9141329d89403ea0
lenovo <lenovo@192.168.1.2>
2020-07-04 07:33:43 +0530

    removed space

--- niranjandemo/src/main/java/com/example/niranjandemo/controller/UserController.java
+++ niranjandemo/src/main/java/com/example/niranjandemo/controller/UserController.java
@@ -1,15 +1,11 @@
 package com.example.niranjandemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
   
-  
-  
-  
-	
-	
   @GetMapping("/")
 	public String viewHomePage() {
 	    
